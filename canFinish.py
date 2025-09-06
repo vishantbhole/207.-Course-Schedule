f117a1be7cbda66aea22c537802e770d8a7a37a9
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-05 21:09:40 -0500

    Update canFinish.py

--- canFinish.py
+++ canFinish.py
@@ -3,3 +3,10 @@
 from typing import List
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        preReq = {i:[] for i in range(numCourses)}
+
+        for course, pre in prerequisites:
+            preReq[course].append(pre)
+
+        visited = set()
