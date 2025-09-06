62935204b65e03da96df50619592b61c1de04a4e
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-05 21:10:18 -0500

    Update canFinish.py

--- canFinish.py
+++ canFinish.py
@@ -10,3 +10,16 @@ class Solution:
             preReq[course].append(pre)
 
         visited = set()
+
+        def dfs(course):
+            if course in visited:
+                return False
+            if preReq[course] == []:
+                return True
+
+            visited.add(course)
+            for preC in preReq[course]:
+                if not dfs(preC): return False
+            visited.remove(course)
+            preReq[course] = []
+            return True
